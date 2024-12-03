4ae341200cfe897fca3830432327f25e002deb88
AlexanderYekat <47818494+AlexanderYekat@users.noreply.github.com>
2024-12-03 13:24:44 +0500

    пероед переносом функциональности с go на php

--- php/inf.php
+++ php/inf.php
@@ -1,65 +1,18 @@
-<html>
-<head>
-    <title>Test</title>
-</head>
-<body>
-<?php
+<?
 
-class TZak 
-{
-    public $cash;
-    public $beznal;
-    public $return;
-    public $cassir;
-    //public function __construct()
-    public $positions;
-}
 
+$zakid = $_GET["zakid"];
+//$zakid = "162279185"; //номер заказа
+$zapr = json_decode(file_get_contents("https://abcp56606.public.api.abcp.ru/cp/order?userlogin=api@abcp56606&userpsw=a7b7b4ec215810e3900225c5de3e917f&number=".$zakid));
 class TCheck
 {
     public $cash;
     public $beznal;
     public $return;
     public $cassir;
-    //public function __construct()
     public $positions;
 }
-
-
-class item {
-    public $description;
-    public $brand;
-    public $quntityFinal;
-    public $priceOut;
-};
-
-
-$fptr = new COM('AddIn.Fptr10');
-//print(fptr.version());
-
-$it1 = new item();
-$it1->description = "наименование товара 1";
-$it1->brand = "brand1";
-$it1->quntityFinal = "3";
-$it1->priceOut = 200;
-
-$it2 = new item();
-$it2->description = "наименование товара 2";
-$it2->brand = "brand2";
-$it2->quntityFinal = "1";
-$it2->priceOut = 250;
-
-$zapr = new TZak();
-$zapr->cash = 0;
-$zapr->beznal = 0;
-$zapr->cassir  = "Иванов";
-$zapr->return = False;
-
-$zapr->positions = array (
-    $it1,
-    $it2
-);
-
+$zapr->cassir = "Исаев Игорь";
 $chcek = new TCheck();
 $chcek->cash = $zapr->cash; //сумма наличнымим - название полей?????????
 $chcek->beznal = $zapr->beznal; //сумма безналом - название полей?????????
@@ -69,14 +22,23 @@ $chcek->positions = array (
 );
 
 
-foreach ($zapr->positions as $item) {
+
+     
+
+ 
+	foreach ($zapr->positions as $item) {
     $newitem["name"] = chop($item->description) . " " . chop($item->brand); //наименование товара
-    $newitem["quantity"] = $item->quntityFinal;
+    $newitem["quantity"] = $item->quantityFinal; 
+	//echo $newitem["quantity"];
     $newitem["price"] = $item->priceOut;
     array_push($chcek->positions, $newitem);
 
 }
 $data_string = json_encode($chcek);
+
+
+
+
 print($data_string ); 
 $url = "localhost:8080";
 $curl = curl_init($url);
@@ -93,11 +55,12 @@ $response = json_decode($json_response, true);
 $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 curl_close($curl);
 if ( $status != 200 ) {
+    //ошибка печати чека
     print("чек не расчпечатан по причине:");
     if ($status == 0) {
         print("нет связи с программой печти чеков");
     }
-    if ($status == 500) {
+    if ($status == 500) {        
         //$response = json_decode($json_response, true);
         print($json_response);        
     }
@@ -106,6 +69,8 @@ if ( $status != 200 ) {
     //$response = json_decode($json_response, true);
     print("Чек распечатан");
 }
+		
+		
+	header("Location: http://ide.rs-truck.ru/kassa_td.php");		
+	
 ?>
-</body>
-</html>
